1d02cc91bde33fe569dff445b3f4506b30d8a30c
Michael Walker <pythonk@email.phoenix.edu>
2018-09-21 08:47:55 -0400

    Fixed typo to Add method in last lecture

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -45,7 +45,7 @@ namespace Treehouse.FitnessFrog.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddPost(string date, string activityId, string duration, string intensity, string exclude, string notes)
+        public ActionResult Add(string date, string activityId, string duration, string intensity, string exclude, string notes)
         {
             ViewBag.Date = date;
             ViewBag.ActivityId = activityId;
